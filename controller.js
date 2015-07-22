50b81a0216cab7bdc38d08c6b4bc1c6e50e26288
mblarrabee <mblarrabee421@gmail.com>
2015-07-21 19:45:09 -0600

    fixed small issue

--- controller.js
+++ controller.js
@@ -1,4 +1,4 @@
-angular.module("userProfiles").controller("userProfiles").controller("mainController", function($scope, mainService){
+angular.module("userProfiles").controller("mainController", function($scope, mainService){
 
 	$scope.users = mainService.getUsers();
 
